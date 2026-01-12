066af1aff96a33c2a66c9f0c67acbc49d2c50097
Alex Dias <ale.xxdias@hotmail.com>
2026-01-12 17:05:14 -0300

    Add files via upload
    Nossa Obra src

---
+++ src/Web-Nossa_Obra/README.md
@@ -0,0 +1,13 @@
+# Responsive Nossa Obra Website
+## [Assista em youtube](https://youtube.com/)
+### Responsive Nossa Obra Website
+
+- Website Responsivo Nossa Obra Design usando HTML CSS And JavaScript
+- Contém animações quando scrolling.
+- Enrolando liso em cada seção.
+- Desenvolvido inicialmente para metodologia Mobile , em seguida para Desktop.
+- Compatível com todos os dispositivos móveis e com uma interface de usuário bonita e agradável.
+
+Um canal para ver mais vídeos sobre este tema. [Ale Web Design](https://www.youtube.com/)
+
+
---
+++ src/Web-Nossa_Obra/assets/js/main.js
@@ -0,0 +1,98 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navclose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle) {
+    navToggle.addEventListener('click' , () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden  */
+if(navclose) {
+    navclose.addEventListener('click' , () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav__link, we remove the show-menu click
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click' , linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // add a class if the botton offset is greater than 50 of the....
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}   
+window.addEventListener('scroll' , bgHeader)
+bgHeader()
+
+/*=============== SWIPER SERVICES ===============*/ 
+const swiperServices = new Swiper('.services__swiper' , {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    slidesPerview: 'auto',
+
+    
+    // navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is higher than 350 iewport height, add the...
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')  
+}
+window.addEventListener('scroll' , scrollUp)
+scrollUp()
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll' , scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = scrollReveal ({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400,
+    //reset: true, // animations repeat
+})
+
+sr.reveal('.home__content, .services__data, .services__swiper, .footer__container')
+sr.reveal('.home__images' , {origin: 'bottom', delay: 1000})
+sr.reveal('.about__images, .contact__img' , {origin: 'left'})
+sr.reveal('.about__data, .contact__data' , {origin: 'right'})
+sr.reveal('.projects__card' , {interval: 100})
